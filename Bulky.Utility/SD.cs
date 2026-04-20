6d1feedd3f479d320d9ae1cdc6bb95d42477e45f
Pais Murama <paizinhomurama@gmail.com>
2026-04-20 20:49:16 +0200

    <modulo 1-2-3>: implementando o modelo MVC e o Razor Pages c#

---
+++ Bulky.Utility/SD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Utility
+{
+    /*
+     Essa classe é uma classe de utilidade (Utility) que pode ser usada para armazenar constantes, métodos auxiliares ou qualquer outra funcionalidade que seja amplamente utilizada em todo o projeto.
+     O nome "SD" é genérico e pode ser substituído por um nome mais descritivo, dependendo do contexto do projeto.
+     */
+    public class SD
+    {
+    }
+}
---
+++ BulkyWebRazor_Temp/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,52 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        public ApplicationDbContext _db;
+        public Category Category { get; set; }
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet(int? id)
+        {
+            if (id != null && id != 0)
+            {
+                Category = _db.Categories.Find(id);
+
+            }
+
+        }
+
+        public IActionResult OnPost(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category = _db.Categories.Find(id);
+
+            if (Category == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(Category);
+            _db.SaveChanges();
+            return RedirectToPage("/Categories/Index");
+
+
+
+        }
+    }
+
+
+}
